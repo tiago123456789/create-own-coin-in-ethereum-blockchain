bbb365a3973a4e5993d600ed00b1d68fb52235dc
Tiago rosa da costa <tiagorosadacost@gmail.com>
2023-08-14 14:47:46 -0300

    Changes to resolve problem pipeline ci when try run project unit tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,21 @@
 require("dotenv").config()
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+let options =  {
   solidity: "0.8.19",
-  // networks: {
-  //   sepolia: {
-  //     url: process.env.URL_INFURE,
-  //     accounts: [process.env.PRIVATE_KEY],
-  //   }
-  // }
+  networks: {
+    sepolia: {
+      url: process.env.URL_INFURE,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
 };
+
+if (process.env.NODE_ENV === 'testing') {
+  options =  {
+    solidity: "0.8.19",
+  }
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = options
