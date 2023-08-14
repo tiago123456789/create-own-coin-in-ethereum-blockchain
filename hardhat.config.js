2b845cbd71838361846debafa8126eb0eac368be
Tiago rosa da costa <tiagorosadacost@gmail.com>
2023-08-14 14:43:36 -0300

    Implemented pipeline ci to project

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-  networks: {
-    sepolia: {
-      url: process.env.URL_INFURE,
-      accounts: [process.env.PRIVATE_KEY],
-    }
-  }
+  // networks: {
+  //   sepolia: {
+  //     url: process.env.URL_INFURE,
+  //     accounts: [process.env.PRIVATE_KEY],
+  //   }
+  // }
 };
